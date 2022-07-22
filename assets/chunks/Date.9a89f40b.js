91c26a0d11962768aead7757692b899e3f568a6a
Corey Raymond DeMarse <1267870+coreydemarse@users.noreply.github.com>
2022-07-22 10:34:00 -0400

    push latest

---
+++ assets/chunks/Date.9a89f40b.js
@@ -0,0 +1,1 @@
+import{o as n,c as o,t as r}from"../app.6caf0b81.js";const c=["datetime"],p={name:"Date",props:{date:Object},setup(t){const e=t;function a(){return new Date(e.date.time).toISOString()}return(i,s)=>(n(),o("time",{datetime:a()},r(t.date.string),9,c))}};export{p as _};
